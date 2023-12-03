83aae821fff23fd75b4516986f0923fe3009954b
Angel Tzankov <32718012+AngelTzankov@users.noreply.github.com>
2023-12-03 14:43:09 +0200

    Create 0-1 subarray.cpp

---
+++ Week 9 - Hash tables/0-1 subarray.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int maxLen(int arr[], int N)
+{
+    unordered_map<int, int> presum;
+ 
+    int sum = 0;
+    int max_len = 0;
+    
+    for (int i = 0; i < N; i++) {
+ 
+        sum += arr[i];
+        if (sum == 0)
+            max_len = i + 1;
+ 
+        if (presum.find(sum) != presum.end()) {
+            max_len = max(max_len, i - presum[sum]);
+        }
+        else {
+            presum[sum] = i;
+        }
+    }
+ 
+    return max_len;
+}
+ 
+int main()
+{
+    int n, arr[1000];
+    cin>>n;
+    for(int i=0;i<n;++i) {
+        cin>>arr[i];
+        if(arr[i] == 0) {
+            arr[i] = -1;
+        }
+    }
+    cout<<maxLen(arr, n);
+ 
+    return 0;
+}
